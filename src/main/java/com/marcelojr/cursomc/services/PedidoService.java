9d2119547bc66954ff1c7fa2eb2cdaf2847f72ac
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2022-08-17 18:36:48 -0300

    Endpoint pedidos/{id}

---
+++ src/main/java/com/marcelojr/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.marcelojr.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelojr.cursomc.domain.Pedido;
+import com.marcelojr.cursomc.repositories.PedidoRepository;
+import com.marcelojr.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não econtrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
