1421048d428b73febc58b49ab81fe51fd97fe9bd
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2022-09-07 20:28:22 -0300

    Crud de Categoria

--- src/main/java/com/marcelojr/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcelojr/cursomc/services/CategoriaService.java
@@ -1,12 +1,19 @@
 package com.marcelojr.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcelojr.cursomc.domain.Categoria;
+import com.marcelojr.cursomc.dto.CategoriaDTO;
 import com.marcelojr.cursomc.repositories.CategoriaRepository;
+import com.marcelojr.cursomc.services.exceptions.DataIntegrityException;
 import com.marcelojr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,10 +22,43 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 					"Objeto não econtrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir Categoria que possui produtos!");
+		}
+				
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 }
