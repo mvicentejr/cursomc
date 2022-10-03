2c62052bc85de2d14788513effbf65ffcc427317
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2022-10-03 19:45:07 -0300

    Validação customizada de Email

--- src/main/java/com/marcelojr/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/marcelojr/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.marcelojr.cursomc.domain.Cliente;
+import com.marcelojr.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
