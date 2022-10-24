eb184f5cd545d466e83e8e25656614f171c55146
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2022-10-24 20:53:30 -0300

    Busca de Produtos por nome e categorias

--- src/main/java/com/marcelojr/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/marcelojr/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.marcelojr.cursomc.repositories;
 
+import java.util.List;
+
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.marcelojr.cursomc.domain.Categoria;
 import com.marcelojr.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
